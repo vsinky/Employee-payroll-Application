5e827313ffa5f041aeb8c9c8f296ed2f54f106e4
vsinky <sinkykumariverma@gmail.com>
2022-01-13 23:53:51 +0530

    Perform validation employee payroll data

---
+++ js/EmployeePayroll.js
@@ -0,0 +1,56 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    salaryOutput();
+    validateName();
+    validateDate();
+
+});
+
+function validateName() {
+    const name = document.querySelector("#name");
+    const textError = document.querySelector(".text-error");
+    name.addEventListener('input', function() {
+        if (name.value.length == 0) {
+            textError.textContent = "";
+            return;
+        }
+        try {
+            (new EmployeePayrollData()).name = name.value;;
+            textError.textContent = "";
+        } catch (e) {
+            console.error(e)
+            textError.textContent = e;
+        }
+    });
+}
+
+function checkDate() {
+    console.log("checking Date");
+    const dateError = document.querySelector('#startDate');
+    try {
+        let date = day.value + " " + month.value + " " + year.value;
+        (new EmployeePayrollData()).startDate = new Date(Date.parse(date));
+        dateError.innerHTML = "";
+    } catch (e) {
+        console.error(e);
+        dateError.innerHTML = e;
+    }
+}
+
+function validateDate() {
+    console.log("validating Date");
+    const day = document.querySelector('#day');
+    const month = document.querySelector('#month');
+    const year = document.querySelector('#year');
+    day.addEventListener('input', checkDate);
+    month.addEventListener('input', checkDate);
+    year.addEventListener('input', checkDate);
+
+}
+const salaryOutput = () => {
+    const salary = document.querySelector('#salary');
+    const output = document.querySelector('.salary-output');
+    output.textContent = salary.value;
+    salary.addEventListener('input', function() {
+        output.textContent = salary.value;
+    });
+}
--- js/EmployeePayrollData.js
+++ js/EmployeePayrollData.js
@@ -27,7 +27,17 @@ class EmployeePayrollData {
     set note(note) { this._note = note; }
 
     get startDate() { return this._startDate; }
-    set startDate(startDate) { this._startDate = startDate; }
+    set startDate(startDate) {
+        let curDate = new Date();
+        if (startDate > curDate)
+            throw "Start date is future date";
+        var diff = Math.abs(curDate.getTime() - startDate.getTime());
+        console.log(diff);
+        if (diff / (1000 * 60 * 60 * 24) > 30)
+            throw "Start date is beyond 30 days";
+        this._startDate = startDate;
+    }
+
 
     toString() {
         const options = { year: 'numeric', month: 'long', day: 'numeric' };

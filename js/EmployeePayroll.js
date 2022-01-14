3e5e87943d8e083b94b0af7deb2498b18f42be26
vsinky <sinkykumariverma@gmail.com>
2022-01-14 22:29:11 +0530

    Employee payroll object to local storage

--- js/EmployeePayroll.js
+++ js/EmployeePayroll.js
@@ -105,3 +105,15 @@ const getInputElementValue = (id) => {
     let value = document.getElementsById(id).value;
     return value;
 }
+
+// uc---4
+function createAndUpdateStorage(employeePayrollData) {
+    let employeePayrollList = JSON.parse(localStorage.getItem("EmployeePayrollList"));
+    if (employeePayrollList != undefined) {
+        employeePayrollList.push(employeePayrollData);
+    } else {
+        employeePayrollList = [employeePayrollData]
+    }
+    alert(employeePayrollList.toString());
+    localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList))
+}

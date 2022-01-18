977668b46c6ba3177f5e4b5bf7aa019e02662137
vsinky <sinkykumariverma@gmail.com>
2022-01-18 20:14:55 +0530

    Ability to view employee payroll from local storage

--- js/home.js
+++ js/home.js
@@ -1,70 +1,45 @@
 window.addEventListener('DOMContentLoaded', (event) => {
+    empPayrollList = getEmployeeDataFromStorage();
+    document.querySelector(".emp-count").textContent = empPayrollList.length;
     createInnerHtml();
 });
-
+const getEmployeeDataFromStorage = () => {
+    return localStorage.getItem('EmployeePayrollList') ? JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];
+}
 const createInnerHtml = () => {
     const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>" +
         "<th>Salary</th><th>Start Date</th><th>Actions</th>";
-    let innerHtml = `${headerHtml}`;
-    let empPayrollList = createEmployeePayrollJSON();
-    for (const empPayrollData of empPayrollList) {
-        innerHtml = `${innerHtml}
-        <tr>
-            <td><img class="profile" alt="" src="${empPayrollData._profilePic}"></td>
-            <td>${empPayrollData._name}</td>
-            <td>${empPayrollData._gender}</td>
-            <td>${getDeptHtml(empPayrollData._department)}</td>
-            <td>${empPayrollData._salary}</td>
-            <td>${empPayrollData._startDate}</td>
-            <td>
-                <img name="${empPayrollData._id}" onclick="remove(this)"src="../assets/icons/delete-black-18dp.svg">
-                <img name="${empPayrollData._id}" onclick="update(this)" src="../assets/icons/create-black-18dp.svg">
-            </td>
-        </tr>        
-    `;
-    }
-    document.querySelector('#table-display').innerHTML = innerHtml;
-}
+    if (empPayrollList.length == 0) return;
 
-const getDeptHtml = (deptList) => {
+    let innerHtml = `${headerHtml}`;
+
+    for (const empPayrollData of empPayrollList) {
+        innerHtml = `${innerHtml}   
+            <tr>
+                <td><img class="profile" alt="" src="${empPayrollData._profilePic}"></td>
+                <td>${empPayrollData._name}</td>
+                <td>${empPayrollData._gender}</td>
+                <td>${getDepartment(empPayrollData._department)}</td>
+                <td>${empPayrollData._salary}</td>
+                <td>${stringifyDate(empPayrollData._startDate)}</td>
+                <td>
+                    <img id="1" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
+                    <img id="1" alt="edit" onclick="update(this)" src="../assets/icons/create-black-18dp.svg">
+                </td>
+           </tr>        
+         `;
+        document.querySelector('#table-display').innerHTML = innerHtml;
+    }
+}
+const getDepartment = (deptList) => {
     let deptHtml = '';
-    for (const dept of deptList) {
-        deptHtml = `${deptHtml}<div class='dept-label'>${dept}</div>`
+    for (let dept of deptList) {
+        deptHtml = `${deptHtml} <div class="dept-label">${dept}</div>`
     }
     return deptHtml;
 }
-
-const createEmployeePayrollJSON = () => {
-    let empPayrollList = [{
-            _name: 'Preety Verma',
-            _gender: 'Female',
-            _department: ['Engineering', 'Sales'],
-            _salary: '500000',
-            _startDate: '29 oct 2019',
-            _note: '',
-            _id: new Date().getTime(),
-            _profilePic: '../assets/profile-images/Ellipse -4.png'
-        },
-        {
-            _name: 'Prakash Kumar',
-            _gender: 'male',
-            _department: ['HR'],
-            _salary: '300000',
-            _startDate: '1 jan 2021',
-            _note: '',
-            _id: new Date().getTime(),
-            _profilePic: '../assets/profile-images/Ellipse -3.png'
-        },
-        {
-            _name: 'Sandeep Verma',
-            _gender: 'male',
-            _department: ['Sales'],
-            _salary: '300000',
-            _startDate: '7 aug 2021',
-            _note: '',
-            _id: new Date().getTime() + 1,
-            _profilePic: '../assets/profile-images/Ellipse -9.png'
-        }
-    ];
-    return empPayrollList;
+const stringifyDate = (date) => {
+    const options = { day: 'numeric', month: 'short', year: 'numeric' };
+    const newDate = !date ? "undefined" : new Date(date).toLocaleDateString('en-GB', options);
+    return newDate;
 }
